777f7b182c8082682813154fe64b9666025509a6
Manuel Escrig <manuelescrig@MacBookPro.home>
2025-06-24 02:01:20 +0200

    Updates dashboard

--- dashboard/index.php
+++ dashboard/index.php
@@ -136,10 +136,6 @@ $currentPage = 'dashboard';
                         <i class="fas fa-eye mr-2"></i>
                         Preview Newsletter
                     </a>
-                    <a href="/dashboard/cron_status.php" class="inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
-                        <i class="fas fa-clock mr-2"></i>
-                        Cron Status
-                    </a>
                 </div>
             </div>
         </div>
