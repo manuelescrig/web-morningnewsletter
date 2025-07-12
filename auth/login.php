e6e6ec9bda2dab96367733c4acc8e9470e9303ec
Manuel Escrig <manuelescrig@MacBookPro.home>
2025-07-12 09:05:28 +0200

    Updates buttons

--- auth/login.php
+++ auth/login.php
@@ -100,10 +100,10 @@ $csrfToken = $auth->generateCSRFToken();
             <div>
                 <button type="submit"
                         class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
-                    <span class="absolute left-0 inset-y-0 flex items-center pl-3">
+                    Sign in
+                    <span class="absolute right-0 inset-y-0 flex items-center pr-3">
                         <i class="fas fa-sign-in-alt text-blue-500 group-hover:text-blue-400"></i>
                     </span>
-                    Sign in
                 </button>
             </div>
         </form>
